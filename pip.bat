9e5097c876bce20613a7126bfe1f9e7b646f5ab0
Rafin <rafin.goku@gmail.com>
2026-08-10 12:24:00 +0600

    Update pip.bat

--- pip.bat
+++ pip.bat
@@ -77,7 +77,7 @@ echo.
 echo [4/4] Verifying dependencies...
 echo.
 
-%PY% -c "import numpy, cv2, PIL, pyaudio, requests, psutil, win32api, win32con, pynput; print('All dependencies OK')"
+%PY% -c "import numpy, opencv-python, PIL, pyaudio, requests, psutil, win32api, win32con, pynput; print('All dependencies OK')"
 
 if errorlevel 1 (
     echo.
